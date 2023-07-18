4384f5cc1cc625daa3f2e6ccc96393746f121788
Ho Tan Hung <tanhungho2002@gmail.com>
2023-07-18 13:37:14 +0700

    Cài đặt babel-plugin-module-resolver ( Import rút gọn )

---
+++ src/Alert.js
@@ -0,0 +1,4 @@
+function myAlert(){
+    alert("Hello");
+}
+export default myAlert
---
+++ src/component/Button.js
@@ -0,0 +1,6 @@
+import myAlert from "~/Alert";
+
+function Button() {
+  return <button onClick={myAlert}>Click Me !</button>;
+}
+export default Button;
